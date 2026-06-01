3f64b9dc4c0edc11193f0327b973be17511826ec
chy3 <843049740@qq.com>
2026-06-01 17:50:06 +0800

    [Feature][SLA] Add Sparse Linear Attention triton operators for A2 and A5
    Co-authored-by: chy3<843049740@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !319 merge dev into dev
    
    [Feature][SLA] Add Sparse Linear Attention triton operators for A2 and A5
    
    Created-by: chy3
    Commit-by: chy3
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20251224
    -->
    
    # Which issue(s) this PR fixes or accomplishes
    [#100](https://gitcode.com/Ascend/MindIE-SD/issues/100) 
    
    # Purpose
    在mindiesd.layers.flash_attn模块中增加 sparse_linear_attn 模块，里面包含SparseLinearAttention算子，与开源仓https://github.com/thu-ml/SLA 接口一致，使用方式即
    
    ```python
    from mindiesd.layers import SparseLinearAttention
    attn = SparseLinearAttention(
        head_dim=128,
        topk=0.2,                 # = 1 - sparsity
        feature_map="softmax",    # options: elu, relu, softmax
        BLKQ=64,
        BLKK=64,
    ).npu()
    
    B, H, L, D = 2, 4, 4096, 128
    q = torch.randn((B, H, L, D), dtype=torch.bfloat16, device='npu')
    k = torch.randn((B, H, L, D), dtype=torch.bfloat16, device='npu')
    v = torch.randn((B, H, L, D), dtype=torch.bfloat16, device='npu')
    
    o = attn(q, k, v)
    ```
    注意：
    - 该PR上传的SparseLinearAttention（SLA）算子以triton-ascend方式实现，支持A2、A3和A5硬件平台；
    - SLA支持BLKK={64， 128}， BLKQ={64， 128}选择；
    - SLA现支持q,k,v的数值类型：bf16，fp16；
    
    # Test Plan
    1、测试调用SparseLinearAttention，传入相应参数，能正常返回结果；
    
    # Test Report
    - 调用
    ![image.png](https://raw.gitcode.com/user-images/assets/8476587/7eebec23-51c1-499a-8d29-323e32f83a95/image.png 'image.png')
    - 返回
    ![image.png](https://raw.gitcode.com/user-images/assets/8476587/c721447c-3a29-4a18-95d1-ab3014020772/image.png 'image.png')
    
    
    See merge request: Ascend/MindIE-SD!319

--- mindiesd/layers/__init__.py
+++ mindiesd/layers/__init__.py
@@ -19,6 +19,7 @@ from .adalayernorm import layernorm_scale_shift
 from .muls_add import muls_add
 from .rope import rotary_position_embedding
 from .fused_moe import fused_moe
+from .flash_attn.sparse_linear_attn import SparseLinearAttention
 
 __all__ = [
     "RMSNorm",
@@ -31,4 +32,5 @@ __all__ = [
     "muls_add",
     "rotary_position_embedding",
     "sparse_attention",
+    "SparseLinearAttention",
 ]
--- mindiesd/layers/flash_attn/__init__.py
+++ mindiesd/layers/flash_attn/__init__.py
@@ -13,3 +13,11 @@
 from .attention_forward import attention_forward
 from .attention_forward_varlen import attention_forward_varlen
 from .sparse_flash_attn import sparse_attention
+from .sparse_linear_attn import SparseLinearAttention
+
+__all__ = [
+    "attention_forward",
+    "attention_forward_varlen",
+    "sparse_attention",
+    "SparseLinearAttention",
+]
